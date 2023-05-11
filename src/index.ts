5124b233333d4ccef7e493676f6f7acba29c2a99
daniel starkey <contact@danielstarkey.co.uk>
2023-05-11 17:05:10 +0100

    feat: allow cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import routes from "./routes";
 
 dotenv.config();
@@ -8,6 +9,12 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 
 app.use("/api/v1", routes);
