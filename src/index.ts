95acb9d487c3eac263e901148eb7ed81a16b94dc
daniel starkey <contact@danielstarkey.co.uk>
2023-05-12 15:57:48 +0100

    chore: remove commented code

--- src/index.ts
+++ src/index.ts
@@ -26,9 +26,6 @@ const app = createExpressServer({
 
 const port = process.env.PORT || 3000;
 
-// app.use(express.json());
-// app.use(fileUpload());
-
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
